a474f6a3f03cbdaa354426b95774b5aeeaa48c74
vikram007007 <alexandervikram80@gmail.com>
2023-09-22 19:15:06 +0530

    model name changed

--- service-provider-svc/src/main/java/org/springboard/dao/ServiceProviderDao.java
+++ service-provider-svc/src/main/java/org/springboard/dao/ServiceProviderDao.java
@@ -1,20 +1,20 @@
 package org.springboard.dao;
 
 import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
-import org.springboard.model.ServiceProvider;
+import org.springboard.model.ServiceProviderModel;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 @EnableScan
-public interface ServiceProviderDao extends CrudRepository<ServiceProvider, String> {
+public interface ServiceProviderDao extends CrudRepository<ServiceProviderModel, String> {
 
-    Optional<ServiceProvider> findById(String id);
+    Optional<ServiceProviderModel> findById(String id);
 
-    List<ServiceProvider> findAll();
+    List<ServiceProviderModel> findAll();
 
-    ServiceProvider save(ServiceProvider serviceProvider);
+    ServiceProviderModel save(ServiceProviderModel serviceProvider);
 
 
 }
