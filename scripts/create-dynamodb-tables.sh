b65a9e64cf8082e2bf8104fd294d1f8e1afeb614
EC2 Default User <ec2-user@ip-172-31-16-100.ec2.internal>
2023-09-22 07:36:39 +0000

    test table and data

--- scripts/create-dynamodb-tables.sh
+++ scripts/create-dynamodb-tables.sh
@@ -10,3 +10,14 @@ aws dynamodb create-table \
 	ReadCapacityUnits=5,WriteCapacityUnits=5 \
 	--region us-east-1
 
+aws dynamodb create-table \
+        --table-name ParticipantTSC \
+        --attribute-definitions AttributeName=id,AttributeType=N \
+        --key-schema AttributeName=id,KeyType=HASH \
+        --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1
+
+aws dynamodb create-table \
+        --table-name ParticipantTSC \
+        --attribute-definitions AttributeName=id,AttributeType=N \
+        --key-schema AttributeName=id,KeyType=HASH \
+        --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1
