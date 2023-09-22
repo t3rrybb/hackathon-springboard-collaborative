7a7f6346d53a9b326583c4e23c52d53b65759bc2
Terry Zhou <t3rrybb@outlook.com>
2023-09-21 23:22:27 -0400

    Create service-provder-svc

---
+++ service-provider-svc/src/test/java/org/springboard/ServiceProviderSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceProviderSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
