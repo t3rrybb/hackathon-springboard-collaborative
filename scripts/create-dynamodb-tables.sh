d18fc5d9141a75f1dc4240a9d01627a556353590
Terry Zhou <t3rrybb@outlook.com>
2023-09-22 00:15:56 +0000

    Add create-dynamodb-tables.sh script

---
+++ scripts/create-dynamodb-tables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+aws dynamodb create-table \
+	--table-name service-provider \
+	--attribute-definitions \
+	AttributeName=id,AttributeType=S \
+	--key-schema \
+	AttributeName=id,KeyType=HASH \
+	--provisioned-throughput \
+	ReadCapacityUnits=5,WriteCapacityUnits=5 \
+	--region us-east-1
