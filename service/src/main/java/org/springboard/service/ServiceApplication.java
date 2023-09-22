ef1522bd7f0067636a8d281bcf76916fab2a04c9
Ashok <ak7195664@gmail.com>
2023-09-22 09:03:29 +0530

    base spring application

---
+++ service/src/main/java/org/springboard/service/ServiceApplication.java
@@ -0,0 +1,15 @@
+package org.springboard.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("org.springboard.controller")
+public class ServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceApplication.class, args);
+	}
+
+}
